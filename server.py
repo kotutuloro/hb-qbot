4d20822be9d0f64fb6cf7f04201871447790562d
Kikelomo Otutuloro <k.otutuloro@gmail.com>
2017-09-20 14:20:45 -0700

    Return slack challenge id

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,15 @@ app = Flask(__name__)
 @app.route('/')
 def test():
     return 'ur here'
+
+
+@app.route('/events', methods=["POST"])
+def events():
+
+    chal_id = request.form.get('challenge')
+    print request.form.items()
+    print chal_id
+    return chal_id
 
 if __name__ == '__main__':
     app.run(port=5050)
