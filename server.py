623e4439d1e30a5ce17f3f651a6d56f74264e6f4
Kikelomo Otutuloro <k.otutuloro@gmail.com>
2017-09-22 19:58:52 -0700

    Track replies to sent messages; Add help and queue changing message options I've never written so much regex in my life?

--- server.py
+++ server.py
@@ -24,7 +24,8 @@ def connect(token):
     Connects to the Slack RTM API with the provided token and gets the WebSocket
     URL returned by the Slack API."""
 
-    payload = {'token': token}
+    payload = {'token': token,
+               'presence_sub': True}
     response = requests.post('https://slack.com/api/rtm.connect', data=payload)
     if response.ok and response.json().get('ok'):
         return response.json().get('url')
@@ -37,6 +38,10 @@ async def receive_events(ws_url):
         # Set global event ID variable to 1
         global event_id_global
         event_id_global = 1
+
+        # Create global dictionary to track message statuses
+        global sent_responses
+        sent_responses = {}
 
         # TODO: Create a ping coro to ping every x seconds and add to event loop
 
@@ -58,9 +63,13 @@ def parse_event(websocket, event):
     msg_type = event.get('type')
     reply = event.get('reply_to')
 
+    # Add previous sent message's success info to dictionary for server replies
     if reply:
+        if not msg_type:
+            global sent_responses
+            sent_responses[reply] = event
+            print(sent_responses)
         return
-        # TODO: Add to dictionary whether previous sent message was successful or not
 
     # Use global event id to for the message ids
     global event_id_global
@@ -76,26 +85,35 @@ def parse_event(websocket, event):
                                             "QBot: Out!",
                                             event_id_global))
 
-    # Further parsing for actual messages
+    # Further parsing for actual messages (ignoring things like channel join msgs)
     if msg_type == 'message' and not event.get('subtype'):
         parse_message(websocket,
-                      event.get('user'),
+                      f"<@{event.get('user')}>",
                       event.get('text'))
 
 
 def parse_message(websocket, user, text):
     """Parse messages to determine and send appropriate response"""
 
-    plain_text = text.strip().lower()
+    potential_new_queue = get_queue_change_message(text)
+
+    # If the message is to print or reform the queue
+    if potential_new_queue is not None:
+        # TODO: Restrict to staff
+        print("I would change the queue now!")
+
+        EVENT_LOOP.create_task(send_message(websocket,
+                                            f"{str(potential_new_queue)}",
+                                            event_id_global))
 
     # If the message indicates that they're on their way to someone:
-    if is_dequeue_message(text):
-        # TODO: Restrict to staff
+    elif is_dequeue_message(text):
+        # TODO: Restrict to staff or self
 
         # Find the user they're trying to pop
         user_to_pop = re.search('<@.+>', text)
         if user_to_pop:
-            user_to_pop = user_to_pop.group(0)
+            user_to_pop = user_to_pop.group()
             EVENT_LOOP.create_task(send_message(websocket,
                                                 f"They're on their way {user_to_pop}",
                                                 event_id_global))
@@ -103,14 +121,26 @@ def parse_message(websocket, user, text):
     # If the message indicates they want to be added to the queue:
     elif is_enqueue_message(text):
         EVENT_LOOP.create_task(send_message(websocket,
-                                            f"Adding to the queue <@{user}>",
+                                            f"Adding to the queue {user}",
                                             event_id_global))
 
-    # If no other conditons met
+    # Return help message for calls to self and stop there
+    elif is_help_message(text):
+        EVENT_LOOP.create_task(send_message(websocket,
+                                            f"Here's where a helpful message would go!",
+                                            event_id_global))
+        # Don't send the queue after this
+        return
+
+    # If no other conditons met, send confusion message and stop there
     else:
         EVENT_LOOP.create_task(send_message(websocket,
-                                            f"Sorry <@{user}>, I didn't understand that!",
+                                            f"Sorry {user}, I didn't understand that.\nType 'qbot help' to see a list of commands.",
                                             event_id_global))
+        # Don't send the queue after this
+        return
+
+    # Print the actual queue here
 
 
 def is_enqueue_message(text):
@@ -124,12 +154,55 @@ def is_enqueue_message(text):
 
 
 def is_dequeue_message(text):
-    """Return where text is message to add self to queue"""
+    """Return whether text is message to add self to queue"""
 
     plain_text = text.strip().lower()
     return (plain_text.startswith('enqueue')
             or plain_text.startswith('enq')
             or plain_text.startswith('nq'))
+
+
+def get_queue_change_message(text):
+    """Return whether text is trying to change the queue"""
+
+    # Construct regex for things like: queue.empty(), q.clear(  ), queue=[],
+    # QUEUE = [ <@User1234> <@U1234here>   ]
+    re_queue_name_alt = '(q|queue)'
+    re_clear_method_alt = '\.(clear|empty)\(\s*\)'
+    re_usernames = '(<@\w*>\s*)*'
+    re_list = f'\s*=\s*\[\s*({ re_usernames })\s*\]'
+
+    search_str = f'{ re_queue_name_alt }({ re_clear_method_alt }|{ re_list })'
+    match = re.search(search_str, text, flags=re.I)
+    # For reference, here's the group order for this regex match
+    # Group 1: 'q' or 'queue' to start (mandatory)
+    # Group 2: '.a_method( )' or ' = [ a list ]' (mandatory)
+    # Group 3: 'clear' or 'empty', if a method
+    # Group 4: all usernames in the list, if a list
+    # Group 5: last username (and whitespace) found, if a list
+
+    if match:
+        # If '.clear()' or '.empty()' was chosen, have an empty queue
+        if match.group(3):
+            new_queue = []
+        # Otherwise find each username in the list
+        else:
+            new_queue = re.findall('<@\w*>', match.group(4))
+
+    # If no match, then not changing the queue
+    else:
+        new_queue = None
+
+    return new_queue
+
+
+def is_help_message(text):
+    """Return whether text is asking for a list of commands"""
+
+    plain_text = text.strip().lower()
+    return ((plain_text.startwith('qbot')
+                or plain_text.startswith('queuebot'))
+            and re.search('\bhelp\b', plain_text))
 
 
 async def send_message(websocket, msg, local_event_id, channel='C77DZM4F9'):
@@ -154,6 +227,10 @@ async def send_message(websocket, msg, local_event_id, channel='C77DZM4F9'):
     # await asyncio.sleep(5)
     await websocket.send(msg_json)
     print(f"Sent message {local_event_id}!")
+
+    # Add the sent message to global sent_responses dict
+    global sent_responses
+    sent_responses[local_event_id] = None
 
 
 if __name__ == '__main__':
