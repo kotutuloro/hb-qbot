8685fab335e1fdf632d28ecaf0c5e05cec0164db
Kikelomo Otutuloro <k.otutuloro@gmail.com>
2017-10-04 10:34:52 -0700

    Testing: 1/(infinity)

---
+++ tests.py
@@ -0,0 +1,24 @@
+import unittest
+
+import qbot
+import myqueue
+import parsing
+
+
+class TestQbot(unittest.TestCase):
+    """Tests for the QBot"""
+    pass
+
+
+class TestQueue(unittest.TestCase):
+    """Tests for the Queue and related classes"""
+    pass
+
+
+class TestParsing(unittest.TestCase):
+    """Tests for parsing helper functions"""
+    pass
+
+
+if __name__ == "__main__":
+    unittest.main()
